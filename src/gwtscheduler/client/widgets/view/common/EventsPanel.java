401b1aaec40ee978aaf7da1a6014e50542f5c47f
Miguel Ping <mping@xplaptop017.(none)>
2010-03-07 18:08:32 +0000

    fixed z-index

--- src/gwtscheduler/client/widgets/view/common/EventsPanel.java
+++ src/gwtscheduler/client/widgets/view/common/EventsPanel.java
@@ -1,6 +1,7 @@
 package gwtscheduler.client.widgets.view.common;
 
 import gwtscheduler.client.modules.AppInjector;
+import gwtscheduler.client.utils.Constants;
 import gwtscheduler.client.widgets.common.event.AppointmentEvent;
 import gwtscheduler.client.widgets.common.event.AppointmentHandler;
 import gwtscheduler.client.widgets.common.event.WidgetResizeEvent;
@@ -17,19 +18,19 @@ public class EventsPanel extends AbstractGridOverlay implements WidgetResizeHand
 
   /**
    * Default constructor.
+   * @param owner the owner
    */
   public EventsPanel() {
     AppInjector.GIN.getInjector().getEventBus().addHandler(AppointmentEvent.getType(), this);
     getElement().getStyle().setPosition(Position.ABSOLUTE);
-    //TODO fix zIndex
-//        getElement().getStyle().setZIndex(Constants.EVENTS_ZINDEX);
   }
 
   @Override
   public void onAddEvent(AppointmentEvent evt) {
     int[] from = calculateLeftTop(evt.from);
-    //    int[] to = calculateLeftTop(evt.to);
-    Label label = new Label("test");
+    //use event factory
+    Label label = new Label("ABCDEFGHIJKL");
+    label.getElement().getStyle().setZIndex(Constants.EVENTS_ZINDEX);
     add(label, from[0], from[1]);
   }
 
